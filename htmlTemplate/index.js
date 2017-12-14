1ff87bf56e69e9261dfdddebfbc4d19359685c9a
yaowenlong <yaowenlong@91taogu.com>
2017-12-14 17:25:17 +0800

    update template

--- htmlTemplate/index.js
+++ htmlTemplate/index.js
@@ -2,13 +2,26 @@ module.exports = function regTemplate (str, obj) {
 
   let pre = 'with(obj){var _str = "";_str +=\''
 
+  // replace last single quote
+  let arr = str.split('%>')
+  arr[arr.length - 1] = arr[arr.length - 1].replace(/'/g, '\\\'')
+  str = arr.join('%>')
+
   let evalStr = str
-    .replace(/<%=(.*?)%>/g, "';_str +=$1;_str +='")
+    // replace first single quote
+    .replace(/([\s\S]*?)<%/m, function (str, $1) { return $1.replace(/'/g, '\\\'') + '<%' })
+    // replace middle single quote
+    .replace(/%>([\s\S]*?)<%/g, function (str, $1) { return '%>' + $1.replace(/'/g, '\\\'') + '<%' })
+    .replace(/[\r\n\t]/g, ' ')
+    .replace(/<%=([\s\S]*?)%>/g, "';_str +=$1;_str +='")
     .replace(/<%/g, "';")
     .replace(/%>/g, "_str+='")
 
   evalStr = pre + evalStr + "';return _str;}"
   var func = new Function ('obj', evalStr)
-
   return func(obj)
+}
+
+function changeQuote (str) {
+  return str.replace(/'/g, '\\\'')
 }
--- htmlTemplate/test.js
+++ htmlTemplate/test.js
@@ -19,9 +19,29 @@ describe('test just variable', function () {
 
 describe('test value and expression', function () {
   
-  it('variable', function () {
+  it('single line', function () {
     const equalTmpl = '<html><body><%= name %><% for(var i = 0; i < 2; i++) {%><div><%= i %></div><% } %></body></html>'
     const afterCompile = '<html><body>hello<div>0</div><div>1</div></body></html>'
+    const obj = {
+      name: 'hello',
+    }
+    let result = regTemplate(equalTmpl, obj)
+    assert.equal(result, afterCompile)
+  })
+
+  it('multi line', function () {
+    const equalTmpl = '<html><body>\n\t<%= name %><% for(var i = 0; i < 2; i++) {%>\n<div><%= i %></div>\n<% } %>\n</body></html>'
+    const afterCompile = '<html><body>  hello <div>0</div>  <div>1</div>  </body></html>'
+    const obj = {
+      name: 'hello',
+    }
+    let result = regTemplate(equalTmpl, obj)
+    assert.equal(result, afterCompile)
+  })
+
+  it('with single quote', function () {
+    const equalTmpl = '<html><body class="\'\'">\n\t<%= name %><% for(var i = 0; i < 2; i++) {%>\n<div onclick="\'\'"><%= i %></div>\n<% } %>\n<div class="\'\'"></div></body></html>'
+    const afterCompile = '<html><body class="\'\'">  hello <div onclick="\'\'">0</div>  <div onclick="\'\'">1</div>  <div class="\'\'"></div></body></html>'
     const obj = {
       name: 'hello',
     }
